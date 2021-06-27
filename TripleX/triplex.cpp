62be3d68376df8c2f64fd90c6d674ac26cf260bd
Raka M.A <rakamuhammadalhafidz12312@gmail.com>
2021-06-27 17:10:58 +0700

    Naming self and Documenting Code

--- TripleX/triplex.cpp
+++ TripleX/triplex.cpp
@@ -4,16 +4,29 @@
 int main(){  // '()' said to pass arguments into the function
     std::cout << "Jadi, lu disuruh sama guru lu untuk ngerjain soal\n";
     //You can also seperate these 2 line with std::endl; instead of \n
-    std::cout << "Terus harus bener semua, gmn cara lu ngebenerin nya?";            //This side is an expression statements. Expressions can be int, String, etc.
-    std::cout << std::endl;
+    std::cout << "Terus harus bener semua, gmn cara lu nyelesainnya?" << std::endl;            //This side is an expression statements. Expressions can be int, String, etc.
+    std::cout << "=============================================================" << std::endl;
 
-    int a = 10;
-    int b = 20;         //This is Declaration statements. Variable must be declared before we expressing it.
-    int c = 30;
+    int CodeA = 10;
+    int CodeB = 20;         //This is Declaration statements. Variable must be declared before we expressing it.
+    int CodeC = 30;
 
-    int total = a + b + c;
 
-    std::cout << total ;
+    std::cout << "- Sebenernya ada 3 angka." << std::endl;
+
+    int CodeTotalTambah = CodeA + CodeB + CodeC;
+    long int CodeTotalKali = CodeA * CodeB * CodeC;
+
+    std::cout << "- Angkanya kalau ditambahakan akan jadi: " << CodeTotalTambah << std::endl;
+    std::cout << "- Angkanya kalo dikalikan jadi: " << CodeTotalKali << std::endl;
+    std::cout << "- Tebak berapakah angkanya? ";
+
+    //Get input variable
+    int PlayerGuess;
+
+
+
+
     return 0; //0 = Program has run succesfully while any other signal or error
 }
 //If it's exited congrats, you have been successful creating C++ program
